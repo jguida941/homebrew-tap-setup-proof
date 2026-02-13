56d6552d37385959fd5fd517adaff49787e37073
Justin Guida <justin.guida@snhu.edu>
2026-02-13 04:51:55 -0500

    Update tap files

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,7 +1,7 @@
 class Hello < Formula
   desc "Program that produces a familiar, friendly greeting"
   homepage "https://www.gnu.org/software/hello/"
-  url "https://ftp.gnu.org/gnu/hello/hello-2.12.1.tar.gz"
+  url "https://ftpmirror.gnu.org/gnu/hello/hello-2.12.1.tar.gz"
   sha256 "8d99142afd92576f30b0cd7cb42a8dc6809998bc5d607d88761f512e26c7db20"
   license "GPL-3.0-or-later"
 
